c8a5f495d55a64972d0a9885fcb3f1b6c0e22ecd
f24170 <xtvb7015@gmail.com>
2025-06-19 03:15:22 +0800

    修正 workflow 與加入資料集下載腳本

---
+++ download_dataset.py
@@ -0,0 +1,12 @@
+import os
+
+# Kaggle 認證（透過 GitHub Secrets）
+kaggle_username = os.getenv('KAGGLE_USERNAME')
+kaggle_key = os.getenv('KAGGLE_KEY')
+
+os.environ['KAGGLE_USERNAME'] = kaggle_username
+os.environ['KAGGLE_KEY'] = kaggle_key
+
+# 自動下載與解壓
+os.system('kaggle datasets download -d paultimothymooney/chest-xray-pneumonia')
+os.system('unzip chest-xray-pneumonia.zip -d chest_xray')
