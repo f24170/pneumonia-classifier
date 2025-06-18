0934f90838aa08947ca2fc8f58521b06c43fcbaa
f24170 <xtvb7015@gmail.com>
2025-06-19 04:00:15 +0800

    更新程式碼及README

--- download_dataset.py
+++ download_dataset.py
@@ -1,12 +1,14 @@
 import os
+import zipfile
 
 # Kaggle 認證（透過 GitHub Secrets）
 kaggle_username = os.getenv('KAGGLE_USERNAME')
 kaggle_key = os.getenv('KAGGLE_KEY')
 
-os.environ['KAGGLE_USERNAME'] = kaggle_username
-os.environ['KAGGLE_KEY'] = kaggle_key
+os.environ['KAGGLE_USERNAME'] = "atim5959"
+os.environ['KAGGLE_KEY'] = "47bc1a9f88700f64a4d07e2ac3956fe7"
 
 # 自動下載與解壓
 os.system('kaggle datasets download -d paultimothymooney/chest-xray-pneumonia')
-os.system('unzip chest-xray-pneumonia.zip -d chest_xray')
+with zipfile.ZipFile("chest-xray-pneumonia.zip", 'r') as zip_ref:
+    zip_ref.extractall(".")
